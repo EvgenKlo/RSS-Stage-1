588d297ae88a6808b0091f3f56e10974b0c67bd2
EvgenKlo <klokovevgen@gmail.com>
2023-04-08 13:38:01 +0500

    merge shelter part3

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,1 +1,344 @@
-console.log('Оценка за работу 100 баллов.\n1. Вёрстка страницы Main соответствует макету при ширине экрана 1280px: +14.\n2. Вёрстка страницы Main соответствует макету при ширине экрана 768px: +14.\n3. Вёрстка страницы Main соответствует макету при ширине экрана 320px: +14.\n4. Вёрстка страницы Pets соответствует макету при ширине экрана 1280px: +6.\n5. Вёрстка страницы Pets соответствует макету при ширине экрана 768px: +6.\n6. Вёрстка страницы Pets соответствует макету при ширине экрана 320px: +6.\n7. Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки, справа от отдельных блоков не появляются белые поля. Весь контент страницы при этом сохраняется: не обрезается и не удаляется: +20.\n8. Верстка резиновая: при плавном изменении размера экрана от 1280px до 320px верстка подстраивается под этот размер, элементы верстки меняют свои размеры и расположение, не наезжают друг на друга, изображения могут менять размер, но сохраняют правильные пропорции (Примеры неправильной и правильной реализации): +8.\n9. При ширине экрана меньше 768px на обеих страницах меню в хедере скрывается, появляется иконка бургер-меню: +4.\n10. Верстка обеих страниц валидная: для проверки валидности вёрстки используйте сервис https://validator.w3.org/ : +8');
+//const { node } = require("webpack");
+
+//console.log('Оценка за работу 100 баллов.\nСтраница Main (60):\n1. Проверка верстки +6;\n2. Вёрстка соответствует макету +35;\n3. Требования к css +7;\n4. Интерактивность элементов +12.\nСтраница Pets(40):\n1. Проверка верстки +6;\n2. Вёрстка соответствует макету +15;\n3. Требования к css +5;\n4. Интерактивность элементов +14.');
+
+const hamb = document.querySelector("#hamb");
+const popup = document.querySelector("#popup");
+const menu = document.querySelector("#menu").cloneNode(1);
+const blackout = document.querySelector('.blackout');
+const body = document.body;
+
+//  Бургер
+hamb.addEventListener("click", hambHandler);
+
+function hambHandler(e) {
+    e.preventDefault();
+    // Переключаем стили элементов при клике
+    popup.classList.toggle("open");
+    hamb.classList.toggle("active");
+    body.classList.toggle("noscroll");
+    blackout.classList.toggle('active');
+    renderPopup();
+  }
+
+function renderPopup() {
+    popup.appendChild(menu);
+}
+
+
+const links = Array.from(menu.children);
+
+// Для каждого элемента меню при клике вызываем ф-ию
+links.forEach((link) => {
+  link.addEventListener("click", closeOnClick);
+});
+
+// Закрытие попапа при клике на меню
+function closeOnClick() {
+  popup.classList.remove("open");
+  hamb.classList.remove("active");
+  body.classList.remove("noscroll");
+  blackout.classList.remove('active');
+};
+
+popup.addEventListener("click", outsideMenuCloseClick);
+
+// Закрытие попапа при клике вне меню
+function outsideMenuCloseClick(e) {
+  if(e.target.className !== "nav-list") {
+    closeOnClick();
+  };
+};
+
+// Закрытие попапа при клике по затемненной области
+
+blackout.addEventListener("click", closeBalckOut);
+
+function closeBalckOut() {
+  blackout.classList.remove('active');
+  closeOnClick();
+};
+
+
+
+//Модальное окно
+
+const petsCards = [
+  {
+    id: 0,
+    "name": "Jennifer",
+    "img": "../../assets/images/img/pets-jennifer.png",
+    "type": "Dog",
+    "breed": "Labrador",
+    "description": "Jennifer is a sweet 2 months old Labrador that is patiently waiting to find a new forever home. This girl really enjoys being able to go outside to run and play, but won't hesitate to play up a storm in the house if she has all of her favorite toys.",
+    "age": "2 months",
+    "inoculations": ["none"],
+    "diseases": ["none"],
+    "parasites": ["none"]
+  },
+  {
+    id: 1,
+    "name": "Sophia",
+    "img": "../../assets/images/img/pets-sophia.png",
+    "type": "Dog",
+    "breed": "Shih tzu",
+    "description": "Sophia here and I'm looking for my forever home to live out the best years of my life. I am full of energy. Everyday I'm learning new things, like how to walk on a leash, go potty outside, bark and play with toys and I still need some practice.",
+    "age": "1 month",
+    "inoculations": ["parvovirus"],
+    "diseases": ["none"],
+    "parasites": ["none"]
+  },
+  {
+    id: 2,
+    "name": "Woody",
+    "img": "../../assets/images/img/pets-woody.png",
+    "type": "Dog",
+    "breed": "Golden Retriever",
+    "description": "Woody is a handsome 3 1/2 year old boy. Woody does know basic commands and is a smart pup. Since he is on the stronger side, he will learn a lot from your training. Woody will be happier when he finds a new family that can spend a lot of time with him.",
+    "age": "3 years 6 months",
+    "inoculations": ["adenovirus", "distemper"],
+    "diseases": ["right back leg mobility reduced"],
+    "parasites": ["none"]
+  },
+  {
+    id: 3,
+    "name": "Scarlett",
+    "img": "../../assets/images/img/pets-scarlet.png",
+    "type": "Dog",
+    "breed": "Jack Russell Terrier",
+    "description": "Scarlett is a happy, playful girl who will make you laugh and smile. She forms a bond quickly and will make a loyal companion and a wonderful family dog or a good companion for a single individual too since she likes to hang out and be with her human.",
+    "age": "3 months",
+    "inoculations": ["parainfluenza"],
+    "diseases": ["none"],
+    "parasites": ["none"]
+  },
+  {
+    id: 4,
+    "name": "Katrine",
+    "img": "../../assets/images/img/pets-katrine.png",
+    "type": "Cat",
+    "breed": "British Shorthair",
+    "description": "Katrine is a beautiful girl. She is as soft as the finest velvet with a thick lush fur. Will love you until the last breath she takes as long as you are the one. She is picky about her affection. She loves cuddles and to stretch into your hands for a deeper relaxations.",
+    "age": "6 months",
+    "inoculations": ["panleukopenia"],
+    "diseases": ["none"],
+    "parasites": ["none"]
+  },
+  {
+    id: 5,
+    "name": "Timmy",
+    "img": "../../assets/images/img/pets-timmy.png",
+    "type": "Cat",
+    "breed": "British Shorthair",
+    "description": "Timmy is an adorable grey british shorthair male. He loves to play and snuggle. He is neutered and up to date on age appropriate vaccinations. He can be chatty and enjoys being held. Timmy has a lot to say and wants a person to share his thoughts with.",
+    "age": "2 years 3 months",
+    "inoculations": ["calicivirus", "viral rhinotracheitis"],
+    "diseases": ["kidney stones"],
+    "parasites": ["none"]
+  },
+  {
+    id: 6,
+    "name": "Freddie",
+    "img": "../../assets/images/img/pets-freddie.png",
+    "type": "Cat",
+    "breed": "British Shorthair",
+    "description": "Freddie is a little shy at first, but very sweet when he warms up. He likes playing with shoe strings and bottle caps. He is quick to learn the rhythms of his human’s daily life. Freddie has bounced around a lot in his life, and is looking to find his forever home.",
+    "age": "2 months",
+    "inoculations": ["rabies"],
+    "diseases": ["none"],
+    "parasites": ["none"]
+  },
+  {
+    id: 7,
+    "name": "Charly",
+    "img": "../../assets/images/img/pets-charly.png",
+    "type": "Dog",
+    "breed": "Jack Russell Terrier",
+    "description": "This cute boy, Charly, is three years old and he likes adults and kids. He isn’t fond of many other dogs, so he might do best in a single dog home. Charly has lots of energy, and loves to run and play. We think a fenced yard would make him very happy.",
+    "age": "8 years",
+    "inoculations": ["bordetella bronchiseptica", "leptospirosis"],
+    "diseases": ["deafness", "blindness"],
+    "parasites": ["lice", "fleas"]
+  }
+]
+
+const clickHandlerOnSliderItem = () => {
+  document.querySelectorAll('.slider_item').forEach(item => {
+    item.addEventListener('click', () => {
+      getModalWindow(petsCards[item.id]);
+    });
+  });
+}
+
+function getModalWindow (date) {
+  let modal;
+  let template = '';
+  modal = document.createElement('div');
+  modal.className = 'overlay';
+  template += `<div class="modal">`;
+    template += `<div class="closeModalBtn">`;
+    template += `</div>`;
+    template += `<div class="modalContent">`;
+      template += `<div class="modalContent_pet">`;
+        template += `<img src="${date.img}" alt="${date.name}">`;
+      template += `</div>`;
+      template += `<div class="modalContent_text">`;
+        template += `<h3 class="modalContent_text-title">${date.name}`;
+        template += `</h3>`;
+        template += `<h4 class="modalContent_text-type-breed">${date.type} - ${date.breed}`;
+        template += `</h4>`;
+        template += `<p class="modalContent_text-description">${date.description}`;
+        template += `</p>`;
+        template += `<ul class="modalContent_text_options">`;
+          template += `<li class="modalContent_text_option"><span>Age: </span><span>${date.age}</span>`;
+          template += `</li>`;
+          template += `<li class="modalContent_text_option"><span>Inoculations: </span><span>${date.inoculations}</span>`;
+          template += `</li>`;
+          template += `<li class="modalContent_text_option"><span>Diseases: </span><span>${date.diseases}</span>`;
+          template += `</li>`;
+          template += `<li class="modalContent_text_option"><span>Parasites: </span><span>${date.parasites}</span>`;
+          template += `</li>`;
+        template += `</ul>`;
+      template += `</div>`;
+    template += `</div>`;
+  template += `</div>`;
+  modal.innerHTML = template;
+  document.querySelector('.our-friends').append(modal);
+  body.classList.add("noscroll");
+
+  const closeBtnModalWindow = document.querySelector('.overlay');
+
+  closeBtnModalWindow.addEventListener('click', (e) => {
+    if (!e.target.closest('.modalContent')) {
+      document.querySelector('.our-friends').removeChild(modal);
+      modal = undefined;
+      body.classList.remove("noscroll");
+    }
+  })
+}
+
+
+//Слайдер
+
+window.onload = function () {
+  console.log('У тебя все получится!')
+
+  clickHandlerOnSliderItem();
+
+  generateArrayNumbers();
+
+  generateSlider();
+}
+
+let randomNumber;
+
+const generateRandomNumber = () => {
+  randomNumber = Math.round(Math.random() * (petsCards.length - 1));
+  return randomNumber;
+};
+
+let randomNumbersArray = [];
+
+const generateArrayNumbers = () => {
+  for (let i = 0; i < petsCards.length; i++) {
+    generateRandomNumber();
+    if(randomNumbersArray.length === 0) {
+      randomNumbersArray.push(randomNumber);
+    } else {
+      if((randomNumbersArray.find((i) => i === randomNumber)) === undefined) {
+        randomNumbersArray.push(randomNumber);
+      } else {
+        i--;
+      }
+    }
+  }
+  return randomNumbersArray.push(randomNumbersArray[0])
+}
+
+const generateSlider = () => {
+  let itemNumber = 0;
+  document.querySelectorAll('.slider_item').forEach(item => {
+    item.id = petsCards[randomNumbersArray[itemNumber]].id;
+    let templateSliderItem = '';
+    templateSliderItem += `<img src="${petsCards[randomNumbersArray[itemNumber]].img}" alt="${petsCards[randomNumbersArray[itemNumber]].name}">`;
+    templateSliderItem += `<h3>${petsCards[randomNumbersArray[itemNumber]].name}</h3>`;
+    templateSliderItem += `<button>Learn more</button>`;
+    item.innerHTML = templateSliderItem;
+    itemNumber++;
+  })
+}
+
+const slider = document.querySelector('.slider');
+const LEFT_BTN_SLIDER = document.querySelector('.arrow-prev');
+const RIGHT_BTN_SLIDER = document.querySelector('.arrow-next');
+const LEFT_BTN_SLIDER_MOBILE = document.querySelector('.mobile-arrow-prev');
+
+const moveLeft = () => {
+  slider.classList.add('transition-left');
+  LEFT_BTN_SLIDER.removeEventListener('click', moveLeft);
+  LEFT_BTN_SLIDER_MOBILE.removeEventListener('click', moveLeft);
+  RIGHT_BTN_SLIDER.removeEventListener('click', moveRight);
+}
+
+LEFT_BTN_SLIDER.addEventListener('click', moveLeft);
+LEFT_BTN_SLIDER_MOBILE.addEventListener('click', moveLeft);
+
+const moveRight = () => {
+  slider.classList.add('transition-right');
+  RIGHT_BTN_SLIDER.removeEventListener('click', moveRight);
+  LEFT_BTN_SLIDER.removeEventListener('click', moveLeft);
+  LEFT_BTN_SLIDER_MOBILE.removeEventListener('click', moveLeft);
+}
+
+RIGHT_BTN_SLIDER.addEventListener('click', moveRight);
+
+slider.addEventListener('animationend', () => {
+  LEFT_BTN_SLIDER.addEventListener('click', moveLeft);
+  LEFT_BTN_SLIDER_MOBILE.addEventListener('click', moveLeft);
+  RIGHT_BTN_SLIDER.addEventListener('click', moveRight);
+  let leftSliderBlock = document.querySelector('.slider_left');
+  let activeSliderBlock = document.querySelector('.slider_active');
+  let rightSliderBlock = document.querySelector('.slider_right');
+  if (slider.classList.contains('transition-left')) {
+    rightSliderBlock.innerHTML = `${activeSliderBlock.innerHTML}`;
+    activeSliderBlock.innerHTML = `${leftSliderBlock.innerHTML}`;
+    generateSliderBlock(leftSliderBlock);
+    slider.classList.remove('transition-left');
+  } else if (slider.classList.contains('transition-right')) {
+    leftSliderBlock.innerHTML = `${activeSliderBlock.innerHTML}`;
+    activeSliderBlock.innerHTML = `${rightSliderBlock.innerHTML}`;
+    generateSliderBlock(rightSliderBlock);
+    slider.classList.remove('transition-right');
+  }
+  clickHandlerOnSliderItem();
+})
+
+const generateSliderBlock = (block) => {
+  if (block.className === 'slider_left') {
+    for (let i = 0; i < 3; i++) {
+      randomNumbersArray.pop();
+      randomNumbersArray.unshift(randomNumbersArray[randomNumbersArray.length - 1]);
+    }
+    for (let i = 0; i < 3; i++) {
+      let template = '';
+      template += `<img src="${petsCards[randomNumbersArray[i]].img}" alt="${petsCards[randomNumbersArray[i]].name}">`;
+      template += `<h3>${petsCards[randomNumbersArray[i]].name}</h3>`;
+      template += `<button>Learn more</button>`;
+      block.children[i].innerHTML = template;
+      block.children[i].id = petsCards[randomNumbersArray[i]].id;
+    }
+  } else {
+    for (let i = 0; i < 3; i++) {
+      randomNumbersArray.shift();
+      randomNumbersArray.push(randomNumbersArray[0]);
+    }
+    for (let i = 0; i < 3; i++) {
+      let template = '';
+      template += `<img src="${petsCards[randomNumbersArray[randomNumbersArray.length - (3 - i)]].img}" alt="${petsCards[randomNumbersArray[randomNumbersArray.length - (3 + i)]].name}">`;
+      template += `<h3>${petsCards[randomNumbersArray[randomNumbersArray.length - (3 - i)]].name}</h3>`;
+      template += `<button>Learn more</button>`;
+      block.children[i].innerHTML = template;
+      block.children[i].id = petsCards[randomNumbersArray[randomNumbersArray.length - (3 - i)]].id;
+    }
+  }
+}
