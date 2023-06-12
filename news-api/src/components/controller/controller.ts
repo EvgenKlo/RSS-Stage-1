4f7c99e29763a7545ce9e26bf80a1e380db6601c
EvgenKlo <klokovevgen@gmail.com>
2023-06-12 21:10:06 +0500

    feat: migrate controller.js to TS

---
+++ news-api/src/components/controller/controller.ts
@@ -0,0 +1,44 @@
+import AppLoader from './appLoader';
+import { Callback } from '../../types/types';
+
+class AppController extends AppLoader {
+    public getSources<T>(callback: Callback<T>): void {
+        super.getResp<T>(
+            {
+                endpoint: 'sources',
+            },
+            callback
+        );
+    }
+
+    public getNews<T>(e: Event, callback: Callback<T>): void {
+        const target = e.target;
+        const newsContainer = e.currentTarget;
+        if (target instanceof HTMLElement && newsContainer instanceof HTMLElement) {
+            let targetClone = target;
+            while (targetClone !== newsContainer) {
+                if (targetClone.classList.contains('source__item')) {
+                    const sourceId = targetClone.getAttribute('data-source-id');
+                    if (newsContainer.getAttribute('data-source') !== sourceId && sourceId !== null) {
+                        newsContainer.setAttribute('data-source', sourceId);
+                        super.getResp(
+                            {
+                                endpoint: 'everything',
+                                options: {
+                                    sources: sourceId,
+                                },
+                            },
+                            callback
+                        );
+                    }
+                    return;
+                }
+                if (targetClone.parentNode instanceof HTMLElement) {
+                    targetClone = targetClone.parentNode;
+                }
+            }
+        }
+    }
+}
+
+export default AppController;
