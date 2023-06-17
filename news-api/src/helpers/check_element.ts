0a35c9b881d6b313e98fe86d31bb37917197175f
EvgenKlo <klokovevgen@gmail.com>
2023-06-17 12:17:19 +0500

    refactor: function in check_elements.ts

--- news-api/src/helpers/check_element.ts
+++ news-api/src/helpers/check_element.ts
@@ -1,5 +1,5 @@
 export function checkElement<T extends HTMLElement> (selector: string, parentElement: Document| HTMLElement | DocumentFragment = document): T {
-  const element: T | null = parentElement.querySelector(selector);
+  const element = parentElement.querySelector<T>(selector);
   if (!element) {
     throw new Error (`Sorry, element ${selector} not found`)
   }
