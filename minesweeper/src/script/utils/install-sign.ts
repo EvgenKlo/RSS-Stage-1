9d84bc1e6ae939d71d799305d0ae6ac84b758532
en.klokov <en.klokov@northis.ru>
2024-10-05 16:21:06 +0500

    refactor: installSign

--- minesweeper/src/script/utils/install-sign.ts
+++ minesweeper/src/script/utils/install-sign.ts
@@ -1,149 +1,30 @@
-import {gameState, gameTimer, settings} from '../index.ts';
+import {gameState, gameTimer} from '../index.ts';
 
 export function installSign() {
     const rows = gameState.allRows;
-    const cells = rows.flatMap(item=>item);
+    const cells = rows.flatMap(item => item);
     cells.forEach(cell => {
         const rowCell = cell.row;
         const numberCell = cell.col;
         let nearBombs = 0;
         if (!cell.hasBomb) {
-            if (rowCell === 0) {
-                if (numberCell === 0) {
-                    if (rows[rowCell][numberCell + 1].hasBomb) {
-                        nearBombs++;
-                    }
-                    if (rows[rowCell + 1][numberCell + 1].hasBomb) {
-                        nearBombs++;
-                    }
-                    if (rows[rowCell + 1][numberCell].hasBomb) {
-                        nearBombs++;
-                    }
-                } else if (numberCell === settings.playingFieldSize - 1) {
-                    if (rows[rowCell][numberCell - 1].hasBomb) {
-                        nearBombs++;
-                    }
-                    if (rows[rowCell + 1][numberCell - 1].hasBomb) {
-                        nearBombs++;
-                    }
-                    if (rows[rowCell + 1][numberCell].hasBomb) {
-                        nearBombs++;
-                    }
-                } else {
-                    if (rows[rowCell][numberCell - 1].hasBomb) {
-                        nearBombs++;
-                    }
-                    if (rows[rowCell + 1][numberCell - 1].hasBomb) {
-                        nearBombs++;
-                    }
-                    if (rows[rowCell + 1][numberCell].hasBomb) {
-                        nearBombs++;
-                    }
-                    if (rows[rowCell + 1][numberCell + 1].hasBomb) {
-                        nearBombs++;
-                    }
-                    if (rows[rowCell][numberCell + 1].hasBomb) {
-                        nearBombs++;
-                    }
+            const left = rows[rowCell] ? rows[rowCell][numberCell - 1] : undefined;
+            const leftTop = rows[rowCell + 1] ? rows[rowCell + 1][numberCell - 1] : undefined;
+            const top = rows[rowCell + 1] ? rows[rowCell + 1][numberCell] : undefined;
+            const rightTop = rows[rowCell + 1] ? rows[rowCell + 1][numberCell + 1] : undefined;
+            const right = rows[rowCell] ? rows[rowCell][numberCell + 1] : undefined;
+            const rightBottom = rows[rowCell - 1] ? rows[rowCell - 1][numberCell + 1] : undefined;
+            const bottom = rows[rowCell - 1] ? rows[rowCell - 1][numberCell] : undefined;
+            const leftBottom = rows[rowCell - 1] ? rows[rowCell - 1][numberCell - 1] : undefined;
+
+            const nearCells = [left, leftTop, top, rightTop, right, rightBottom, bottom, leftBottom];
+
+            nearCells.map(item => {
+                if (item && item.hasBomb) {
+                    nearBombs++;
                 }
-            } else if (rowCell === settings.playingFieldSize - 1) {
-                if (numberCell === 0) {
-                    if (rows[rowCell][numberCell + 1].hasBomb) {
-                        nearBombs++;
-                    }
-                    if (rows[rowCell - 1][numberCell + 1].hasBomb) {
-                        nearBombs++;
-                    }
-                    if (rows[rowCell - 1][numberCell].hasBomb) {
-                        nearBombs++;
-                    }
-                } else if (numberCell === settings.playingFieldSize - 1) {
-                    if (rows[rowCell][numberCell - 1].hasBomb) {
-                        nearBombs++;
-                    }
-                    if (rows[rowCell - 1][numberCell - 1].hasBomb) {
-                        nearBombs++;
-                    }
-                    if (rows[rowCell - 1][numberCell].hasBomb) {
-                        nearBombs++;
-                    }
-                } else {
-                    if (rows[rowCell][numberCell - 1].hasBomb) {
-                        nearBombs++;
-                    }
-                    if (rows[rowCell - 1][numberCell - 1].hasBomb) {
-                        nearBombs++;
-                    }
-                    if (rows[rowCell - 1][numberCell].hasBomb) {
-                        nearBombs++;
-                    }
-                    if (rows[rowCell - 1][numberCell + 1].hasBomb) {
-                        nearBombs++;
-                    }
-                    if (rows[rowCell][numberCell + 1].hasBomb) {
-                        nearBombs++;
-                    }
-                }
-            } else {
-                if (numberCell === 0) {
-                    if (rows[rowCell - 1][numberCell].hasBomb) {
-                        nearBombs++;
-                    }
-                    if (rows[rowCell - 1][numberCell + 1].hasBomb) {
-                        nearBombs++;
-                    }
-                    if (rows[rowCell][numberCell + 1].hasBomb) {
-                        nearBombs++;
-                    }
-                    if (rows[rowCell + 1][numberCell + 1].hasBomb) {
-                        nearBombs++;
-                    }
-                    if (rows[rowCell + 1][numberCell].hasBomb) {
-                        nearBombs++;
-                    }
-                } else if (numberCell === settings.playingFieldSize - 1) {
-                    if (rows[rowCell - 1][numberCell].hasBomb) {
-                        nearBombs++;
-                    }
-                    if (rows[rowCell - 1][numberCell - 1].hasBomb) {
-                        nearBombs++;
-                    }
-                    if (rows[rowCell][numberCell - 1].hasBomb) {
-                        nearBombs++;
-                    }
-                    if (rows[rowCell + 1][numberCell - 1].hasBomb) {
-                        nearBombs++;
-                    }
-                    if (rows[rowCell + 1][numberCell].hasBomb) {
-                        nearBombs++;
-                    }
-                } else {
-                    if (rows[rowCell - 1][numberCell - 1].hasBomb) {
-                        nearBombs++;
-                    }
-                    if (rows[rowCell - 1][numberCell].hasBomb) {
-                        nearBombs++;
-                    }
-                    if (rows[rowCell - 1][numberCell + 1].hasBomb) {
-                        nearBombs++;
-                    }
-                    if (rows[rowCell][numberCell - 1].hasBomb) {
-                        nearBombs++;
-                    }
-                    if (rows[rowCell][numberCell + 1].hasBomb) {
-                        nearBombs++;
-                    }
-                    if (rows[rowCell + 1][numberCell - 1].hasBomb) {
-                        nearBombs++;
-                    }
-                    if (rows[rowCell + 1][numberCell].hasBomb) {
-                        nearBombs++;
-                    }
-                    if (rows[rowCell + 1][numberCell + 1].hasBomb) {
-                        nearBombs++;
-                    }
-                }
-            }
+            })
+
         }
         if (nearBombs !== 0) {
             cell.addClassName(`int${nearBombs}`);
